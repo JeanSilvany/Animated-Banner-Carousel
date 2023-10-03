eb93a4a9ccef7797bb3a3e3da751b11d103ee603
jeansilvany <jeansilvany@hotmail.com>
2023-10-02 21:51:45 -0300

    refact to styled components

--- styles.ts
+++ styles.ts
@@ -1,4 +1,9 @@
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
+
+export const WIDTH = Dimensions.get("screen").width;
+export const IMAGE_WIDTH = WIDTH * 0.88;
+export const SPACING = WIDTH * 0.02;
 
 export const Container = styled.SafeAreaView`
   flex: 1;
@@ -6,4 +11,19 @@ export const Container = styled.SafeAreaView`
   align-items: center;
 `;
 
-export const Image = styled.Image``;
+export const CardSpacer = styled.View`
+  width: ${(WIDTH - IMAGE_WIDTH) / 3}px;
+  background-color: red;
+  height: 210px;
+  border-radius: 8px;
+`;
+
+export const Image = styled.Image`
+  width: ${IMAGE_WIDTH}px;
+  height: 210px;
+  border-radius: 8px;
+`;
+
+export const Separator = styled.View`
+  width: ${SPACING}px;
+`;
