5a24e37159835b5ad856982dd8cb2aba4b19088e
jeansilvany <jeansilvany@hotmail.com>
2023-10-02 23:17:19 -0300

    fix layout

--- App.tsx
+++ App.tsx
@@ -1,4 +1,11 @@
-import { FlatList, Platform } from "react-native";
+import {
+  Button,
+  FlatList,
+  Platform,
+  TouchableOpacity,
+  ViewToken,
+} from "react-native";
+
 import {
   CardSpacer,
   Container,
@@ -7,6 +14,12 @@ import {
   SPACING,
   Separator,
 } from "./styles";
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import { useEffect, useRef, useState } from "react";
 
 const data = [
   "https://picsum.photos/1920/1080",
@@ -25,23 +38,40 @@ const data = [
 export default function App() {
   const dataImages = ["left-spacer", ...data, "right-spacer"];
 
+  const scrollX = useSharedValue(0);
+  const flatListRef = useAnimatedRef<FlatList>();
+  const flatListIndex = useSharedValue(1);
+
+  const autoPlay = true;
+  const timingDelay = 5000;
+
+  const OnScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollX.value = event.contentOffset.x;
+    },
+  });
+
   const renderItem = ({ item, index }) => {
     if (index === 0 || index === dataImages.length - 1) {
       return <CardSpacer />;
     }
 
-    return <Image source={{ uri: item }} />;
+    return (
+      <TouchableOpacity activeOpacity={0.7}>
+        <Image source={{ uri: item }} />
+      </TouchableOpacity>
+    );
   };
 
   const snapToOffsets = dataImages.map((_, index) => {
     return (
-      (index === 0 ? SPACING : 0) +
+      (index === 1 ? SPACING : 0) +
       index * (IMAGE_WIDTH + SPACING) -
       (index === dataImages.length - 1 ? SPACING : 0)
     );
   });
 
-  const getItemLayout = (_, index) => {
+  const getItemLayout = (_: any, index: number) => {
     return {
       length: IMAGE_WIDTH + SPACING,
       offset:
@@ -54,24 +84,67 @@ export default function App() {
 
   const decelerationRate = Platform.OS === "ios" ? "fast" : 0.66;
 
+  const onViewableItemsChanged = useRef(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      if (viewableItems[0].index !== null) {
+        flatListIndex.value = viewableItems[0].index;
+      }
+    }
+  );
+
+  const handleScrollToNext = () => {
+    const ITEMS_SIZE = dataImages.length - 2;
+    if (flatListIndex.value < ITEMS_SIZE) {
+      console.log("IF");
+      flatListRef.current.scrollToIndex({
+        index: flatListIndex.value + 1,
+        animated: true,
+      });
+
+      flatListIndex.value += 1;
+    } else {
+      console.log("ELSE");
+
+      flatListRef.current.scrollToIndex({
+        index: 0,
+        animated: true,
+      });
+
+      flatListIndex.value = 0;
+    }
+  };
+
+  useEffect(() => {
+    if (autoPlay) {
+      setInterval(() => {
+        handleScrollToNext();
+      }, timingDelay);
+    }
+  }, [autoPlay]);
+
   return (
     <Container>
-      <FlatList
+      <Animated.FlatList
+        ref={flatListRef}
         data={dataImages}
         renderItem={renderItem}
         horizontal
         contentContainerStyle={{
           alignItems: "center",
+          gap: SPACING,
         }}
         snapToAlignment="center"
         initialNumToRender={3}
+        onScroll={OnScroll}
+        onViewableItemsChanged={onViewableItemsChanged.current}
         snapToOffsets={snapToOffsets}
         getItemLayout={getItemLayout}
         decelerationRate={decelerationRate}
         scrollEventThrottle={16}
         showsHorizontalScrollIndicator={false}
-        ItemSeparatorComponent={() => <Separator />}
+        // ItemSeparatorComponent={() => <Separator />}
       />
+      <Button title="Next" onPress={handleScrollToNext} />
     </Container>
   );
 }
